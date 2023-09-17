daf10c1c05253ae39d8ad83c5b60cd7e6c404728
Zeljko Lojic <zlojic-ext@unifonic.com>
2023-09-17 11:48:58 +0200

    Skip duplicate entries from input

--- src/main/kotlin/com/unifonic/authzMapper/model/input/LegacyJson.kt
+++ src/main/kotlin/com/unifonic/authzMapper/model/input/LegacyJson.kt
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 
 
-class LegacyJson @JsonCreator constructor(
+data class LegacyJson @JsonCreator constructor(
     @JsonProperty("actionName") @JsonAlias("action", "scope") val scope: String,
     @JsonProperty("resourceName") @JsonAlias("resource") val resource: String,
     @JsonProperty("roleName") @JsonAlias("role") val role: String,
